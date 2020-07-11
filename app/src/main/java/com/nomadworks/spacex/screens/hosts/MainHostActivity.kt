c89b88dea7f9ff032948a0c3480143b38cd15183
Dukho <dukhochoi@gmail.com>
2020-07-11 22:43:21 +1000

    Add dependency for nav controller

---
+++ app/src/main/java/com/nomadworks/spacex/screens/hosts/MainHostActivity.kt
@@ -0,0 +1,12 @@
+package com.nomadworks.spacex.screens.hosts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nomadworks.spacex.R
+
+class MainHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_host)
+    }
+}
