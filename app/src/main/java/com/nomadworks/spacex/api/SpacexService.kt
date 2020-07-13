58f0893f85c8a44f543bbbd92a355e0896da35a5
Dukho <dukhochoi@gmail.com>
2020-07-13 23:04:35 +1000

    add payload api definition

--- app/src/main/java/com/nomadworks/spacex/api/SpacexService.kt
+++ app/src/main/java/com/nomadworks/spacex/api/SpacexService.kt
@@ -1,8 +1,10 @@
 package com.nomadworks.spacex.api
 
-import com.nomadworks.spacex.api.model.Launch
+import com.nomadworks.spacex.api.model.launch.Launch
+import com.nomadworks.spacex.api.model.payload.Payload
 import retrofit2.http.GET
 import retrofit2.Response
+import retrofit2.http.Path
 
 interface SpacexService {
     @GET("launches/latest")
@@ -10,4 +12,10 @@ interface SpacexService {
 
     @GET("launches/latest")
     suspend fun getLatestLaunchResponse(): Response<Launch>
+
+    @GET("payloads")
+    suspend fun getAllPayloads(): List<Payload>
+
+    @GET("payloads/{id}")
+    suspend fun getPayloadById(@Path("id") payloadId: String): Payload
 }
